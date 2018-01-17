188b16e3030807aea2215b61dcf08e396865e1fb
alexandre-mace <alex-mace@hotmail.fr>
2018-01-17 14:54:24 +0100

    Modification on index.php

--- web/index.php
+++ web/index.php
@@ -6,4 +6,12 @@ $twig = new Twig_Environment($loader, array(
     'cache' => false
 ));
 
-echo $twig->render('home.html.twig');
+$router = New Router;
+
+Router::addRoute;
+
+Router::getCurrentRoute;
+	//try every route by parsing
+	//route match to test it and return route parameters
+
+Route::call
