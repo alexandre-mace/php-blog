d7ebd71b45075a0a1e41c0440e60b4e00f92287a
alexandre-mace <alex-mace@hotmail.fr>
2018-02-12 10:57:31 +0100

    database into singleton

--- app/Database.php
+++ app/Database.php
@@ -6,11 +6,21 @@ class Database
 {
 
 	private $pdo;
+	private static $_databaseInstance
 
 
-	public function __construct()
+	public function __construct($host, $dbName, $user, $password)
 	{
-		$this->pdo = new PDO("mysql:dbname=$db;host=$host", $username, $password); // A modifier
+		$this->pdo = new \PDO("mysql:dbname=$db;host=$host", $username, $password); // A modifier
+	}
+
+	public static function getInstance()
+	{
+		if (is_null(self::$_databaseInstance)) {
+			self::$_databaseInstance = new App();
+		}
+
+		return self::$_databaseInstance;
 	}
 
 	public function getPdo()
